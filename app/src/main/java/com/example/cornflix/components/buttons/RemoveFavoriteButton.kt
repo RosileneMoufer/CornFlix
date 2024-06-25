6fca4c4f521dd621b81185f0c283777c01c2eb56
RosileneMoufer <rosilenemoufer@gmail.com>
2024-06-25 11:35:05 -0300

    Description Screen, SaveButton, TrailerButton.

--- app/src/main/java/com/example/cornflix/components/buttons/RemoveFavoriteButton.kt
+++ app/src/main/java/com/example/cornflix/components/buttons/RemoveFavoriteButton.kt
@@ -1,2 +1,10 @@
 package com.example.cornflix.components.buttons
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RemoveFavoriteButton() {
+}
--- app/src/main/java/com/example/cornflix/components/buttons/SaveButton.kt
+++ app/src/main/java/com/example/cornflix/components/buttons/SaveButton.kt
@@ -1,2 +1,54 @@
 package com.example.cornflix.components.buttons
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cornflix.ui.theme.textColor
+import com.example.cornflix.ui.theme.transparent
+
+@Composable
+fun SaveButton() {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(transparent)
+            .border(2.dp, color = textColor, shape = RoundedCornerShape(8.dp))
+            .clickable { /* TODO */ },
+        contentAlignment = Alignment.Center
+    ) {
+        Row(modifier = Modifier.padding(start = 12.dp, top = 12.dp, end = 16.dp, bottom = 12.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Filled.Favorite,
+                contentDescription = "Salvar",
+                tint = textColor
+            )
+            Text(
+                text = "Salvar", style = TextStyle(
+                    color = textColor,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W600
+                )
+            )
+        }
+    }
+}
